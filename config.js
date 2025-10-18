2c61ef9c5b67401d8e9e058938dd80409fdfb5ab
Nguyễn Hữu Văn Lâm <160982567+VlamIT-ut@users.noreply.github.com>
2025-10-19 00:40:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc Huyền';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Ngọc Huyền của tớ 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau hehe';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
