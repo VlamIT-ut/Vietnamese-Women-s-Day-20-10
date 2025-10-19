bf918af8f5b2b2238903339194da5874bcb23491
vanlam <nguyenhuuvanlam17@gmail.com>
2025-10-20 00:23:24 +0700

    debug

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Ngọc Huyền';
-// const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc Ngọc Huyền của tớ 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau hehe';
